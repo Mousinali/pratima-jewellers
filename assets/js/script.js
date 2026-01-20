6c5f0a20b81fb7e60204bf13198dce318c60349d
Mousin <work.mousin@gmail.com>
2026-01-20 14:04:20 +0530

    project done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,42 @@
-// var nav = document.querySelector(".navbar");
-// window.onscroll = function () {
-//     if (document.documentElement.scrollTop > 20) {
-//         nav.classList.add("scroll-on");
-//     }
-//     else {
-//         nav.classList.remove("scroll-on");
-//     }
-// }
+var nav = document.querySelector("header");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("scroll-on");
+    }
+    else {
+        nav.classList.remove("scroll-on");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const modal = document.getElementById("applyModal");
+    const backdrop = document.getElementById("applyBackdrop");
+    const modalBox = modal.querySelector("div");
+
+    window.openApplyModal = function () {
+        modal.classList.remove("hidden");
+        backdrop.classList.remove("hidden");
+
+        setTimeout(() => {
+            backdrop.classList.remove("opacity-0");
+            modalBox.classList.remove("scale-95", "opacity-0");
+            modal.classList.add("flex");
+        }, 10);
+    };
+
+    window.closeApplyModal = function () {
+        backdrop.classList.add("opacity-0");
+        modalBox.classList.add("scale-95", "opacity-0");
+
+        setTimeout(() => {
+            modal.classList.add("hidden");
+            modal.classList.remove("flex");
+            backdrop.classList.add("hidden");
+        }, 200);
+    };
+});
+
+
 
 document.querySelectorAll(".dropdown-toggle").forEach((btn) => {
     btn.addEventListener("click", () => {
@@ -21,6 +51,7 @@ document.querySelectorAll(".dropdown-toggle").forEach((btn) => {
             icon.classList.add("rotate-180");
         }
     });
+
 });
 
 gsap.registerPlugin(ScrollTrigger);
